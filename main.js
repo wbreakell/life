e94abf030fcf34752b78e38b269f6916b29c98e9
Walter Breakell <walterjbreakell@gmail.com>
2025-03-01 23:35:51 -0800

    Configure the canvas

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ if (!adapter) {
 }
 
 const device = await adapter.requestDevice();
+
+// Configure the canvas
+const context = canvas.getContext("webgpu");
+const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+context.configure({
+  device: device,
+  format: canvasFormat,
+});
