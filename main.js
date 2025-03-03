f9116a2ad0691bb760f5a92d4cc6e046eb207cb1
Walter Breakell <walterjbreakell@gmail.com>
2025-03-02 22:37:08 -0800

    Clear the canvas with a solid color

--- main.js
+++ main.js
@@ -14,3 +14,19 @@ context.configure({
   device: device,
   format: canvasFormat,
 });
+
+// Clear the canvas with a render pass
+const encoder = device.createCommandEncoder();
+
+const pass = encoder.beginRenderPass({
+  colorAttachments: [{
+    view: context.getCurrentTexture().createView(),
+    loadOp: "clear",
+    clearValue: { r: 0, g: 0, b: 0.4, a: 1 },
+    storeOp: "store",
+  }],
+});
+
+pass.end();
+
+device.queue.submit([encoder.finish()]);
