710be10e9489563a1d8615c97fee5836c6d744d7
Walter Breakell <walterjbreakell@gmail.com>
2025-03-04 22:20:04 -0800

    Draw a colored square to the canvas

--- main.js
+++ main.js
@@ -15,6 +15,66 @@ context.configure({
   format: canvasFormat,
 });
 
+// Create a buffer with the vertices for a single cell
+const vertices = new Float32Array([
+//   X,    Y
+  -0.8, -0.8, // Triangle 1
+   0.8, -0.8,
+   0.8,  0.8,
+  -0.8, -0.8, // Triangle 2
+   0.8,  0.8,
+  -0.8,  0.8,
+]);
+const vertexBuffer = device.createBuffer({
+  label: "Cell vertices",
+  size: vertices.byteLength,
+  usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+});
+device.queue.writeBuffer(vertexBuffer, 0, vertices);
+
+const vertexBufferLayout = {
+  arrayStride: 8,
+  attributes: [{
+    format: "float32x2",
+    offset: 0,
+    shaderLocation: 0,
+  }],
+};
+
+// Create the shader that will render the cells
+const cellShaderModule = device.createShaderModule({
+  label: "Cell shader",
+  code: `
+    @vertex
+    fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
+      return vec4f(pos, 0, 1);
+    }
+
+    @fragment
+    fn fragmentMain() -> @location(0) vec4f {
+      return vec4f(1, 0, 0, 1);
+    }
+  `,
+});
+
+// Create a pipeline that renders the cell
+const cellPipeline = device.createRenderPipeline({
+  label: "Cell pipeline",
+  layout: "auto",
+  vertex: {
+    module: cellShaderModule,
+    entryPoint: "vertexMain",
+    buffers: [vertexBufferLayout],
+  },
+  fragment: {
+    module: cellShaderModule,
+    entryPoint: "fragmentMain",
+    targets: [{
+      format: canvasFormat,
+    }],
+  },
+});
+
 // Clear the canvas with a render pass
 const encoder = device.createCommandEncoder();
 
@@ -26,6 +86,10 @@ const pass = encoder.beginRenderPass({
     storeOp: "store",
   }],
 });
+
+pass.setPipeline(cellPipeline);
+pass.setVertexBuffer(0, vertexBuffer);
+pass.draw(vertices.length / 2); // 6 vertices
 
 pass.end();
 
