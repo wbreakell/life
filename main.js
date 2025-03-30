99b999fb51e801c668797bfeff53fe18df9b3868
Walter Breakell <walterjbreakell@gmail.com>
2025-03-29 18:28:06 -0700

    Change the color of each square based on its cell coordinate

--- main.js
+++ main.js
@@ -52,6 +52,7 @@ const cellShaderModule = device.createShaderModule({
     };
     struct VertexOutput {
       @builtin(position) pos: vec4f,
+      @location(0) cell: vec2f,
     };
 
     @group(0) @binding(0) var<uniform> grid: vec2f;
@@ -64,12 +65,14 @@ const cellShaderModule = device.createShaderModule({
       let gridPos = (input.pos + 1) / grid - 1 + cellOffset;
       var output: VertexOutput;
       output.pos = vec4f(gridPos, 0, 1);
+      output.cell = cell;
       return output;
     }
 
     @fragment
-    fn fragmentMain() -> @location(0) vec4f {
-      return vec4f(1, 0, 0, 1);
+    fn fragmentMain(input: VertexOutput) -> @location(0) vec4f {
+      let c = input.cell / grid;
+      return vec4f(c, 1-c.x, 1);
     }
   `,
 });
